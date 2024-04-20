bf92bfc2239aedfd76d6e1248ae84a98ad68b58b
ishtiaqSamdani <82057297+ishtiaqSamdani@users.noreply.github.com>
2024-04-20 13:32:38 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,25 +4,11 @@ echo "Requesting registration URL at '${registration_url}'"
 
 payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PERSONAL_TOKEN}" ${registration_url})
 export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
-# v3
-# RUNNER_CPU="2"
-# RUNNER_MEMORY="2GB"
-# RUNNER_NAME="flash-${RUNNER_CPU}cpu-${RUNNER_MEMORY}"
-# v4
-RUNNER_CPU="2"
-RUNNER_MEMORY="4GB"
-RUNNER_NAME="flash-${RUNNER_CPU}cpu-${RUNNER_MEMORY}"
-
-echo "Runner Name: ${RUNNER_NAME}"
-
-random_string() {
-    cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 10 | head -n 1
-}
 
 ./config.sh \
-    --name "flash-${RUNNER_NAME}-$(random_string)" \
+    --name $(hostname) \
     --token ${RUNNER_TOKEN} \
-    --labels ${RUNNER_NAME} \
+    -- labels my-runner \
     --url https://github.com/${GITHUB_OWNER}/${GITHUB_REPOSITORY} \
     --work "/work" \
     --unattended \
@@ -35,8 +21,6 @@ remove() {
 trap 'remove; exit 130' INT
 trap 'remove; exit 143' TERM
 
-./run.sh --once "$*" &
+./run.sh "$*" &
 
 wait $!
-
-remove

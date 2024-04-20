84809e4aa6422885c89cb7354719ad715b9039d8
ishtiaqSamdani <82057297+ishtiaqSamdani@users.noreply.github.com>
2024-04-20 13:33:41 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,30 @@ echo "Requesting registration URL at '${registration_url}'"
 
 payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PERSONAL_TOKEN}" ${registration_url})
 export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
+# v3
+# RUNNER_CPU="2"
+# RUNNER_MEMORY="2GB"
+# RUNNER_NAME="flash-${RUNNER_CPU}cpu-${RUNNER_MEMORY}"
+# v4
+RUNNER_CPU="2"
+RUNNER_MEMORY="4GB"
+RUNNER_NAME_ARGUMENT_PASSED=$1
+RUNNER_NAME="flash-${RUNNER_CPU}cpu-${RUNNER_MEMORY}"
+
+if [ -n "$RUNNER_NAME_ARGUMENT_PASSED" ]; then
+    RUNNER_NAME=$RUNNER_NAME_ARGUMENT_PASSED
+fi
+
+echo "Runner Name: ${RUNNER_NAME}"
+
+random_string() {
+    cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 5 | head -n 1
+}
 
 ./config.sh \
-    --name $(hostname) \
+    --name "${RUNNER_NAME}-$(random_string)" \
     --token ${RUNNER_TOKEN} \
-    -- labels my-runner \
+    --labels ${RUNNER_NAME} \
     --url https://github.com/${GITHUB_OWNER}/${GITHUB_REPOSITORY} \
     --work "/work" \
     --unattended \
@@ -21,6 +40,8 @@ remove() {
 trap 'remove; exit 130' INT
 trap 'remove; exit 143' TERM
 
-./run.sh "$*" &
+./run.sh --once "$*" &
 
 wait $!
+
+remove

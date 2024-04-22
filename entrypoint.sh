1b5628af363060a974f59b6cbea4be5cb5cbb076
ishtiaqSamdani <82057297+ishtiaqSamdani@users.noreply.github.com>
2024-04-22 17:52:18 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-registration_url="https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/actions/runners/registration-token"
+registration_url="https://api.github.com/orgs/${GITHUB_OWNER}/actions/runners/registration-token"
 echo "Requesting registration URL at '${registration_url}'"
 
 payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PERSONAL_TOKEN}" ${registration_url})
@@ -11,30 +11,26 @@ export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
 # v4
 RUNNER_CPU="2"
 RUNNER_MEMORY="4GB"
-RUNNER_NAME_ARGUMENT_PASSED=$1
 RUNNER_NAME="flash-${RUNNER_CPU}cpu-${RUNNER_MEMORY}"
-
-if [ -n "$RUNNER_NAME_ARGUMENT_PASSED" ]; then
-    RUNNER_NAME=$RUNNER_NAME_ARGUMENT_PASSED
-fi
 
 echo "Runner Name: ${RUNNER_NAME}"
 
 random_string() {
-    cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 5 | head -n 1
+    cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 10 | head -n 1
 }
 
 ./config.sh \
-    --name "${RUNNER_NAME}-$(random_string)" \
+    --name "flash-${RUNNER_NAME}-$(random_string)" \
     --token ${RUNNER_TOKEN} \
     --labels ${RUNNER_NAME} \
-    --url https://github.com/${GITHUB_OWNER}/${GITHUB_REPOSITORY} \
+    --url https://github.com/${GITHUB_OWNER} \
     --work "/work" \
     --unattended \
-    --replace
+    --replace \
+    --ephemeral
 
 remove() {
-    ./config.sh remove --unattended --token "${RUNNER_TOKEN}"
+    ./config.sh remove --token "${RUNNER_TOKEN}"
 }
 
 trap 'remove; exit 130' INT
